91c79d3608446e7ff3e4697a474164842b91e8d3
JUANES5545 <juanes.reich@gmail.com>
2025-04-29 12:40:17 -0500

    Temporal implementation

--- app/src/main/java/com/example/vinilosmobileapp/model/dto/AlbumCreateDTO.kt
+++ app/src/main/java/com/example/vinilosmobileapp/model/dto/AlbumCreateDTO.kt
@@ -1,8 +1,5 @@
 package com.example.vinilosmobileapp.model.dto
 
-import com.example.vinilosmobileapp.model.Comment
-import com.example.vinilosmobileapp.model.Track
-
 data class AlbumCreateDTO(
     val name: String,
     val cover: String,
