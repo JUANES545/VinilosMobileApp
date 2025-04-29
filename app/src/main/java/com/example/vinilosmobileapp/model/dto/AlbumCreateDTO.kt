441d1343cc59bd34c6fb85edf3efab81bb1edddf
Miguel Alejandro Gómez Alarcón <miguel.gomezalarcon@mercadolibre.com.co>
2025-04-29 14:18:05 -0500

    fix tabs

--- app/src/main/java/com/example/vinilosmobileapp/model/dto/AlbumCreateDTO.kt
+++ app/src/main/java/com/example/vinilosmobileapp/model/dto/AlbumCreateDTO.kt
@@ -1,18 +1,15 @@
 package com.example.vinilosmobileapp.model.dto
 
-import com.example.vinilosmobileapp.model.Comment
-import com.example.vinilosmobileapp.model.Track
-
 data class AlbumCreateDTO(
-    val name: String,
-    val cover: String,
+    val name:        String,
+    val cover:       String,
     val releaseDate: String,
     val description: String,
-    val genre: String,
+    val genre:       String,
     val recordLabel: String
 )
 
 data class TrackCreateDTO(
-    val name: String,
+    val name:     String,
     val duration: String
 )
