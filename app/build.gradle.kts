4c03e5ed32fd8be503fa40de7bf86a111f7886d8
Jazmin Natalia Cordoba <jn.cordobap1@uniandes.edu.co>
2025-05-25 19:10:37 -0500

    add test expresso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-
 }
 
 android {
@@ -23,6 +22,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        // compose = true // Si vas a usar Compose, descomenta esta línea
     }
 
     buildTypes {
@@ -34,19 +34,20 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        // compose = true
-    }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -55,7 +56,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -67,15 +67,25 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.androidx.junit.ktx)
     implementation(libs.androidx.swiperefreshlayout)
+
+    // Pruebas unitarias
     testImplementation(libs.junit)
+
+    // Pruebas instrumentadas
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.mockito.android)
+    androidTestImplementation("org.mockito:mockito-android:5.11.0") // Usa la versión más reciente
+
+
+    // Fragment testing (solo en debug)
+    debugImplementation("androidx.fragment:fragment-testing:1.8.7")
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // Other implementations
+    // Otras dependencias
     implementation(libs.navigation.fragment.ktx)
     implementation(libs.navigation.ui.ktx)
     implementation(libs.material)
@@ -84,12 +94,9 @@ dependencies {
     implementation(libs.coil.core)
     implementation(libs.shimmer)
 
-
     // Retrofit
     implementation(libs.retrofit)
     implementation(libs.gson)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.retrofit.converter.scalars)
-
-
 }
